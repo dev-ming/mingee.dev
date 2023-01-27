4decc6fcc89c1f87de22811bb00856716f748d70
Jenna <ruaaaa@ajou.ac.kr>
2023-01-27 18:24:56 +0900

    :lipstick: update header ui

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,7 +4,14 @@ import "./Header.css";
 function header(props) {
   return (
     <div className="Header">
-      <p>header</p>
+      <div>Logo</div>
+      <nav>
+        <ul className="navigation-menu">
+          <li style={{ color: "#00FF58" }}>About</li>
+          <li style={{ color: "#FFFF00" }}>Project</li>
+          <li style={{ color: "#00CEFF" }}>Contact</li>
+        </ul>
+      </nav>
     </div>
   );
 }
